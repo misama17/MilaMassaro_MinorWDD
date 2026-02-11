57c8495dd0eed6c0d8a65d4429ad9ecfc4bfe98f
misama17 <milamassaro17@gmail.com>
2026-02-11 16:15:02 +0100

    update of the check-out

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // ************************************
-// -------------- CONSTANTS -----------
+// -------- */ MARK: CONSTANTS --------
 // ************************************
 
 // ---------- 3D CAROUSEL ----------
@@ -11,6 +11,10 @@ nextButton.addEventListener("click", toNext);
 previousButton.addEventListener("click", toPrev);
 
 
+
+// ************************************
+// ---------*/ MARK: VARIABLES --------
+// ************************************
 let charms = [
     "fav_hobby",
     "fav_animal",
@@ -27,34 +31,59 @@ let leerdoelen = [
     "leerdoel 3"
 ]
 
+let leerdoelenTekst = [
+    "Ik kan gebruikerservaring vertalen naar bewuste keuzes in HTML-structuur, interactie en visuele hiërarchie, zodat de interface intuïtief aanvoelt zonder uitleg.",
+    "Ik wil ontdekken of front-end development mijn toekomstige vakgebied zou kunnen zijn, door ervaring op te doen met HTML, CSS en JavaScript, en te reflecteren op mijn eigen interesse, motivatie en voldoening bij het werken aan websites.",
+    "Ik ga experimenteren met animaties met CSS (en JavaScript) en ontdek zo hoe ik unieke, opvallende websites kan maken."
+]
 
-
-// ************************************
-// -------------- VARIABLES -----------
-// ************************************
 
 // ------------- API: 311 -------------
-let base = "https://fdnd.directus.app/items"
-let endpoint = "/person/311"
-let url = base + endpoint
+async function insertCharacter(charm) {
+    let base = "https://fdnd.directus.app/items"
+    let endpoint = "/person/311"
+    let url = base + endpoint
+
+    let response = await fetch(url)
+
+    let characterInfo = await response.json();
+
+    let valueCharm = characterInfo.data[charm]
+    document.querySelector("section:nth-of-type(2) p").textContent = valueCharm
+
+    // `<img src="${characterInfo.image}" alt="${characterInfo.name}">`
+}
 
 
 
-// insertCharacter();
+async function getMinorMensen(charm) {
+    let base = "https://fdnd.directus.app/items"
+    let endpoint = "/person?filter[fav_tag][_nempty]"
+    let url = base + endpoint
 
-// async function insertCharacter() {
-//     let response = await fetch(url)
+    let response = await fetch(url)
 
-//     let characterInfo = await response.json();
-//     // console.log(characterInfo.name)
+    let responseJSON = await response.json();
 
-//     let characterHTML = 
-// 		`<h3>Mensen met hetzelfde sterrenbeeld:</h3>
-//      <p>${characterInfo.fav_animal}</p>
-//      <img src="" alt="">`
-// }
+    let deMinorMensen = responseJSON.data;
 
+    deMinorMensen.forEach(eenMinorMens => {
+        console.log(eenMinorMens)
 
+        let deMinorMensenHTML = 
+        `<li>
+        <h2>${eenMinorMens.name}</h2>
+        <p>${eenMinorMens.fav_tag}</p>
+        <img src = "${eenMinorMens.avatar} alt="${eenMinorMens.name}">
+        </li>`
+
+        // beforeend: hij voegt het voor het einde van de ul toe.
+        // afterbegin: als eerste toegevoegd.
+        // !!! Je mag nu de HTML weghalen, maar de ul moet blijven staan! dus je hebt een lege ul in je HTML.
+        deLijst.insertAdjacentHTML("beforeend", deMinorMensenHTML)
+    }
+    ) 
+}
 
 
 
@@ -85,6 +114,8 @@ function getInfo(getal) {
     let remainder = getal % 10
 
     let thema = document.querySelector("h2");
+    let medestudenten = document.querySelector("h3");
+    let definitie = document.querySelector("section:nth-of-type(2) p:nth-of-type(1)");
 
     if (remainder < 0 ) {
         remainder = remainder + 10
@@ -97,10 +128,15 @@ function getInfo(getal) {
 
         console.log("nummer:" + nummer)
         
-        let themaText = 
-        `<h2>${charm}</h2>`
-
+        // h2
+        let themaText = `<h2>${charm}</h2>`
         thema.innerHTML = themaText;
+
+        insertCharacter(charm);
+
+        // h3
+        let medestudentenText = `<h3>Mensen met zelfde ${charm}:</h3>`
+        medestudenten.innerHTML = medestudentenText;
     } 
     else {
         let ldNummer = nummer - 7
@@ -108,10 +144,23 @@ function getInfo(getal) {
         console.log("leerdoel nummer:" + ldNummer)
         // leerdoelen
         let leerdoel = leerdoelen[ldNummer];
+        let leerdoelTekst = leerdoelenTekst[ldNummer];
         
-        let themaText = 
-        `<h2>${leerdoel}</h2>`
-        
+        // h2
+        let themaText = `<h2>${leerdoel}</h2>`
         thema.innerHTML = themaText;
+
+        // h3
+        let medestudentenText = `<h3></h3>`
+        medestudenten.innerHTML = medestudentenText;
+
+        // definitie p
+        let definitieText = `<p>${leerdoelTekst}</p>`
+        definitie.innerHTML = definitieText;
+
+        // Avatar
+        let avatar = `<img></img>`
     }
 }
+
+getInfo(0) 
