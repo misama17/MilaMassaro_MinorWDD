20112a1b18e2b8a804bf8f88e68e7f63c7559e10
misama17 <milamassaro17@gmail.com>
2026-02-11 14:33:06 +0100

    another update

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,21 @@ nextButton.addEventListener("click", toNext);
 previousButton.addEventListener("click", toPrev);
 
 
+let charms = [
+    "fav_hobby",
+    "fav_animal",
+    "vibe_emoji",
+    "fav_season",
+    "shoe_size",
+    "residency",
+    "fav_movie"
+]
+
+let leerdoelen = [
+    "leerdoel 1",
+    "leerdoel 2",
+    "leerdoel 3"
+]
 
 
 
@@ -25,20 +40,19 @@ let url = base + endpoint
 
 
 
-insertCharacter();
+// insertCharacter();
 
-async function insertCharacter() {
-    let response = await fetch(url)
+// async function insertCharacter() {
+//     let response = await fetch(url)
 
-    let characterInfo = await response.json();
-    console.log(characterInfo.name)
+//     let characterInfo = await response.json();
+//     // console.log(characterInfo.name)
 
-    let characterHTML = 
-		`<h2>Sterrenbeeld</h2>
-     <h3>Mensen met hetzelfde sterrenbeeld:</h3>
-     <p>${characterInfo.fav_animal}</p>
-     <img src="" alt="">`
-}
+//     let characterHTML = 
+// 		`<h3>Mensen met hetzelfde sterrenbeeld:</h3>
+//      <p>${characterInfo.fav_animal}</p>
+//      <img src="" alt="">`
+// }
 
 
 
@@ -47,7 +61,7 @@ async function insertCharacter() {
 
 
 // ************************************
-// -------------- FUNCTIONS -----------
+// -------- */ MARK: FUNCTIONS --------
 // ************************************
 
 // ---------- 3D CAROUSEL ----------
@@ -55,10 +69,49 @@ function toNext() {
   let curExtra = getComputedStyle(carouselContainer).getPropertyValue("--extra");
   let newExtra = parseInt(curExtra) - 1;  
   carouselContainer.style.setProperty("--extra", newExtra);
+
+  getInfo(newExtra)
 }
 
 function toPrev() {
   let curExtra = getComputedStyle(carouselContainer).getPropertyValue("--extra");
   let newExtra = parseInt(curExtra) + 1;  
   carouselContainer.style.setProperty("--extra", newExtra);
+
+  getInfo(newExtra)
+}
+
+function getInfo(getal) {
+    let remainder = getal % 10
+
+    let thema = document.querySelector("h2");
+
+    if (remainder < 0 ) {
+        remainder = remainder + 10
+    }
+
+    let nummer = 9 - remainder
+
+    if  (nummer <= 6) {
+        let charm = charms[nummer]
+
+        console.log("nummer:" + nummer)
+        
+        let themaText = 
+        `<h2>${charm}</h2>`
+
+        thema.innerHTML = themaText;
+    } 
+    else {
+        let ldNummer = nummer - 7
+
+        console.log("leerdoel nummer:" + ldNummer)
+        // leerdoelen
+        let leerdoel = leerdoelen[ldNummer];
+        
+        let themaText = 
+        `<h2>${leerdoel}</h2>`
+        
+        thema.innerHTML = themaText;
+    }
 }
